a2aaaeb7c7aa3cd79d8ec6085238bd727445734b
andrewliao11 <andrewliao11@gmail.com>
2016-09-22 10:04:06 +0800

    complete gradient

--- dni.py
+++ dni.py
@@ -4,18 +4,16 @@ from utils import linear
 # assume the input vector is 1D vector
 class synthetic_grad():
 
-	def __init__(self, inputs_shape, name='synthetic_grad'):
+	def __init__(self, inputs, name='synthetic_grad'):
 
 		self.var = {}
 		# B, H
-		self.inputs_shape = inputs_shape
-		self.inputs = tf.placeholder('float32', self.inputs_shape)
-		self.gt_gradient = tf.placeholder('float32', self.input_shape)
+		self.inputs_shape = inputs.get_shape()
 		with tf.variable_scope(name):
 			self.h1, self.var['l1_w'], self.var['l1_b'] = linear(inputs, 4000, weights_initializer=tf.zeros_initializer, 
-												biases_initializer=tf.zeros_initializer, activation_fn=tf.relu, batch_norm=True, name='l1')
+												biases_initializer=tf.zeros_initializer, activation_fn=tf.nn.relu, batch_norm=True, name='l1')
 			self.synthetic_grad, self.var['l2_w'], self.var['l2_b'] = linear(self.h1, self.inputs_shape[1], weights_initializer=tf.zeros_initializer,
-												biases_initializer=tf.zeros_initializer, activation_fn=tf.relu, batch_norm=True, name='l2')
+												biases_initializer=tf.zeros_initializer, activation_fn=tf.nn.relu, batch_norm=True, name='l2')
 
-		self.loss = tf.reduce_mean(tf.square(self.synthetic_grad - self.gt_gradient))
+
 
